724e961c8a242b5651ad443d54a7078f1bedae8f
mikeGorDomino <m.gorin.airlife@gmail.com>
2021-03-05 10:41:12 +0200

    print on console

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world Java");
+        System.out.println("I like coding in Java and Intellij IDEA");
     }
 
 
